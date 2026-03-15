da81eb66182db54e9b00558cf73283a539ee698a
pablo_acebron <pabloacebron@hotmail.es>
2026-03-15 19:40:44 +0100

    Modificación de la clase en la rama develop

--- Cuadrado.java
+++ Cuadrado.java
@@ -3,6 +3,8 @@ package figuras;
 import java.awt.Color;
 
 public class Cuadrado extends Rectangulo {
+
+    //Este comentario es la modificación que hay que hacer a una clase en el ejercicio 2, para después hacer el push desde la rama develop
     public Cuadrado(double x, double y, Color color, double lado) {
         super(x, y, color, lado, lado);
     }
